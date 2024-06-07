039b0aa35ba13b743443acad9495eab520e408b0
Matt Mundell <matt@mundell.me>
2024-06-07 13:33:16 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { parser } from "./syntax.grammar";
 import { LRLanguage, LanguageSupport } from "@codemirror/language";
+import { styleTags, tags } from "@lezer/highlight";
+
+let props;
+
+props = [ styleTags({ Addition: tags.inserted,
+                      Deletion: tags.deleted,
+                      Command: tags.meta,
+                      Index: tags.meta,
+                      NewFile: tags.meta,
+                      OldFile: tags.meta,
+                      Filename: tags.meta,
+                      Location: tags.meta }) ];
 
 export const diffLanguage = LRLanguage.define({
-  parser: parser.configure({}),
+  parser: parser.configure({ props: props }),
 });
 
 export function diff() {
