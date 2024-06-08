e794615cfb30f4f214ad2addcaea789e287918ff
Matt Mundell <matt@mundell.me>
2024-06-08 02:38:02 +0200

    Add style tags for highlighting

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
 import { parser } from "./syntax.grammar";
 import { LRLanguage, LanguageSupport } from "@codemirror/language";
+import { styleTags, Tag, tags } from "@lezer/highlight";
+
+export const customTags = { diffFilename: Tag.define(tags.meta),
+                            diffNewfile: Tag.define(tags.meta),
+                            diffOldfile: Tag.define(tags.meta) }
+
+let props;
+
+props = [ styleTags({ Addition: tags.inserted,
+                      Deletion: tags.deleted,
+                      Command: tags.meta,
+                      Index: tags.meta,
+                      Commit: tags.meta,
+                      NewFile: customTags.diffNewfile,
+                      OldFile: customTags.diffOldfile,
+                      Filename: customTags.diffFilename,
+                      Location: tags.meta,
+                      Linerange: tags.meta }) ];
 
 export const diffLanguage = LRLanguage.define({
-  parser: parser.configure({}),
+  parser: parser.configure({ props: props }),
 });
 
 export function diff() {
